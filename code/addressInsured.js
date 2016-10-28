1d8283aa8b2910c6d48b0bca07bfe0d48f7fe74a
somename <ntelk@allstate.com>
2016-10-27 20:10:59 -0500

    asdf

--- code/addressInsured.js
+++ code/addressInsured.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Title from './title';
 import Card from './card';
- ControlBar from './controlBar';
+import ControlBar from './controlBar';
 //import AddressInsuredForm from './addressInsuredForm';
 import PropertyDetails from './propertyDetails';
 
